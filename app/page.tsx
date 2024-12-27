180708bc5434bd56286ad6d44d3b58f250268a4d
RabGH <rabii.ghais@gmail.com>
2024-12-27 20:44:57 +0200

    Refractor: move components folder to root, implement shiny button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import MaxWidthWrapper from "@/app/components/max-width-wrapper"
-import { Heading } from "@/app/components/heading"
+import { Check } from "lucide-react"
+
+import MaxWidthWrapper from "@/components/max-width-wrapper"
+import { Heading } from "@/components/heading"
+import { ShinyButton } from "@/components/shiny-button"
 
 export default function Home() {
   return (
@@ -15,6 +18,32 @@ export default function Home() {
                   Delivered to Your Discord
                 </span>
               </Heading>
+            </div>
+            <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+              Ping Bonzo, easiest way to monitor your SaaS. Get instant
+              notifications for{" "}
+              <span className="font-semibold text-gray-700">
+                sales, new users, or any other event
+              </span>{" "}
+              sent directly to your discord
+            </p>
+
+            <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start">
+              {[
+                "Real-time Discord alerts for critical events",
+                "Buy once, use forever",
+                "Track sales, new users, or any other event",
+              ].map((item, index) => (
+                <li key={index} className="flex gap-1.5 items-center text-left">
+                  <Check className="size-5 shrink-0 text-brand-700" />
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <div className="w-full max-w-80">
+              <ShinyButton className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl">
+                Start For Free Today
+              </ShinyButton>
             </div>
           </div>
         </MaxWidthWrapper>
