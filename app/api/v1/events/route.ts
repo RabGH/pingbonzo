99214f9fe10604e81629e145f2c15f0788d7c7aa
RabGH <rabii.ghais@gmail.com>
2025-01-02 07:41:53 +0200

    SERVER: get categories by name, category page content using tabs

--- app/api/v1/events/route.ts
+++ app/api/v1/events/route.ts
@@ -17,190 +17,209 @@ const REQUEST_VALIDATOR = z
   .strict()
 
 export const POST = async (req: NextRequest) => {
-  const authHeader = req.headers.get("Authorization")
-
-  if (!authHeader) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
-  }
-
-  if (!authHeader.startsWith("Bearer ")) {
-    return NextResponse.json(
-      {
-        message: "Invalid auth header format. Expected: 'Bearer [API_KEY]'",
-      },
-      { status: 401 }
-    )
-  }
-
-  const apiKey = authHeader.split(" ")[1]
-
-  if (!apiKey || apiKey.trim() === "") {
-    return NextResponse.json(
-      {
-        message: "Invalid API key",
-      },
-      { status: 401 }
-    )
-  }
-
-  const user = await db.user.findUnique({
-    where: { apiKey },
-    include: {
-      eventCategories: true,
-    },
-  })
-
-  if (!user) {
-    return NextResponse.json(
-      {
-        message: "Invalid API key",
-      },
-      { status: 401 } // unauthorized
-    )
-  }
-
-  if (!user.discordId) {
-    return NextResponse.json(
-      {
-        message: "Please enter your discord ID in your account settings.",
-      },
-      { status: 403 } // forbidden, allowed but can't serve them cause we need disc id first.
-    )
-  }
-
-  // quota
-  const currentDate = new Date()
-  const currentMonth = currentDate.getMonth() + 1
-  const currentYear = currentDate.getFullYear()
-
-  const quota = await db.quota.findUnique({
-    // get quota, refreshes at the start of every month
-    where: {
-      userId: user.id,
-      month: currentMonth,
-      year: currentYear,
-    },
-  })
-
-  const quotaLimit =
-    user.plan === "FREE"
-      ? FREE_QUOTA.maxEventsPerMonth
-      : PRO_QUOTA.maxEventsPerMonth
-
-  if (quota && quota.count >= quotaLimit) {
-    return NextResponse.json(
-      {
-        message:
-          "Monthly quota reached. Please upgrade your plan for more events.",
-      },
-      { status: 429 } // payment required http status
-    )
-  }
-
-  const discord = new DiscordClient(process.env.DISCORD_BOT_TOKEN)
-
-  const dmChannel = await discord.createDM(user.discordId)
-
-  // await discord.sendEmbed(dmChannel.id, {
-  //   title: "Hello! From PingBonzo!",
-  // })
-
-  let requestData: unknown
   try {
-    requestData = await req.json()
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: "Invalid JSON request body",
-      },
-      { status: 400 }
-    )
-  }
+    const authHeader = req.headers.get("Authorization")
 
-  const validationResult = REQUEST_VALIDATOR.parse(requestData)
+    if (!authHeader) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+    }
 
-  const category = user.eventCategories.find(
-    (category) => category.name === validationResult.category
-  )
+    if (!authHeader.startsWith("Bearer ")) {
+      return NextResponse.json(
+        {
+          message: "Invalid auth header format. Expected: 'Bearer [API_KEY]'",
+        },
+        { status: 401 }
+      )
+    }
 
-  if (!category) {
-    return NextResponse.json(
-      {
-        message: `You dont have a category named "${validationResult.category}"`,
-      },
-      { status: 404 }
-    )
-  }
+    const apiKey = authHeader.split(" ")[1]
 
-  const eventData = {
-    title: `${category.emoji || "🔔"} ${category.name.charAt(0).toUpperCase() + category.name.slice(1)}`,
-    description:
-      validationResult.description ||
-      `a new ${category.name} event has occured.`,
-    color: category.color,
-    timestamp: new Date().toISOString(),
-    fields: Object.entries(validationResult.fields || {}).map(
-      ([key, value]) => {
-        return { name: key, value: String(value), inline: true }
-      }
-    ),
-  }
+    if (!apiKey || apiKey.trim() === "") {
+      return NextResponse.json(
+        {
+          message: "Invalid API key",
+        },
+        { status: 401 }
+      )
+    }
 
-  const event = await db.event.create({
-    data: {
-      name: category.name,
-      formattedMessage: `${eventData.title}\n\n${eventData.description}`,
-      userId: user.id,
-      fields: validationResult.fields || {},
-      eventCategoryId: category.id,
-    },
-  })
-
-  try {
-    await discord.sendEmbed(dmChannel.id, eventData)
-
-    await db.event.update({
-      where: {
-        id: event.id,
-      },
-      data: {
-        deliveryStatus: "DELIVERED",
+    const user = await db.user.findUnique({
+      where: { apiKey },
+      include: {
+        eventCategories: true,
       },
     })
 
-    await db.quota.upsert({
-      where: { userId: user.id, month: currentMonth, year: currentYear },
-      update: { count: { increment: 1 } },
-      create: {
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "Invalid API key",
+        },
+        { status: 401 } // unauthorized
+      )
+    }
+
+    if (!user.discordId) {
+      return NextResponse.json(
+        {
+          message: "Please enter your discord ID in your account settings.",
+        },
+        { status: 403 } // forbidden, allowed but can't serve them cause we need disc id first.
+      )
+    }
+
+    // quota
+    const currentDate = new Date()
+    const currentMonth = currentDate.getMonth() + 1
+    const currentYear = currentDate.getFullYear()
+
+    const quota = await db.quota.findUnique({
+      // get quota, refreshes at the start of every month
+      where: {
         userId: user.id,
         month: currentMonth,
         year: currentYear,
-        count: 1,
-      },
-    })
-  } catch (error) {
-    await db.event.update({
-      where: {
-        id: event.id,
-      },
-      data: {
-        deliveryStatus: "FAILED",
       },
     })
 
-    console.log(error)
+    const quotaLimit =
+      user.plan === "FREE"
+        ? FREE_QUOTA.maxEventsPerMonth
+        : PRO_QUOTA.maxEventsPerMonth
+
+    if (quota && quota.count >= quotaLimit) {
+      return NextResponse.json(
+        {
+          message:
+            "Monthly quota reached. Please upgrade your plan for more events.",
+        },
+        { status: 429 } // payment required http status
+      )
+    }
+
+    const discord = new DiscordClient(process.env.DISCORD_BOT_TOKEN)
+
+    const dmChannel = await discord.createDM(user.discordId)
+
+    // await discord.sendEmbed(dmChannel.id, {
+    //   title: "Hello! From PingBonzo!",
+    // })
+
+    let requestData: unknown
+    try {
+      requestData = await req.json()
+    } catch (error) {
+      return NextResponse.json(
+        {
+          message: "Invalid JSON request body",
+        },
+        { status: 400 }
+      )
+    }
+
+    const validationResult = REQUEST_VALIDATOR.parse(requestData)
+
+    const category = user.eventCategories.find(
+      (category) => category.name === validationResult.category
+    )
+
+    if (!category) {
+      return NextResponse.json(
+        {
+          message: `You dont have a category named "${validationResult.category}"`,
+        },
+        { status: 404 }
+      )
+    }
+
+    const eventData = {
+      title: `${category.emoji || "🔔"} ${category.name.charAt(0).toUpperCase() + category.name.slice(1)}`,
+      description:
+        validationResult.description ||
+        `a new ${category.name} event has occured.`,
+      color: category.color,
+      timestamp: new Date().toISOString(),
+      fields: Object.entries(validationResult.fields || {}).map(
+        ([key, value]) => {
+          return { name: key, value: String(value), inline: true }
+        }
+      ),
+    }
+
+    const event = await db.event.create({
+      data: {
+        name: category.name,
+        formattedMessage: `${eventData.title}\n\n${eventData.description}`,
+        userId: user.id,
+        fields: validationResult.fields || {},
+        eventCategoryId: category.id,
+      },
+    })
+
+    try {
+      await discord.sendEmbed(dmChannel.id, eventData)
+
+      await db.event.update({
+        where: {
+          id: event.id,
+        },
+        data: {
+          deliveryStatus: "DELIVERED",
+        },
+      })
+
+      await db.quota.upsert({
+        where: { userId: user.id, month: currentMonth, year: currentYear },
+        update: { count: { increment: 1 } },
+        create: {
+          userId: user.id,
+          month: currentMonth,
+          year: currentYear,
+          count: 1,
+        },
+      })
+    } catch (error) {
+      await db.event.update({
+        where: {
+          id: event.id,
+        },
+        data: {
+          deliveryStatus: "FAILED",
+        },
+      })
+
+      console.log(error)
+
+      return NextResponse.json(
+        {
+          message: "Error processing event",
+          eventId: event.id,
+        },
+        { status: 500 } // internal server error
+      )
+    }
+
+    return NextResponse.json({
+      message: "Event processed succesfully",
+      eventId: event.id,
+    })
+  } catch (error) {
+    console.error(error)
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        { status: 422 } // inappropriate content, is not valid
+      )
+    }
 
     return NextResponse.json(
       {
-        message: "Error processing event",
-        eventId: event.id,
+        message: "Internal server error",
       },
-      { status: 500 } // internal server error
+      { status: 500 }
     )
   }
-
-  return NextResponse.json({
-    message: "Event processed succesfully",
-    eventId: event.id,
-  })
 }
--- app/dashboard/category/[name]/category-page-content.tsx
+++ app/dashboard/category/[name]/category-page-content.tsx
@@ -1,11 +1,16 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
 import { useQuery } from "@tanstack/react-query"
 
 import { EventCategory } from "@prisma/client"
 
 import { EmptyCategoryState } from "./empty-category-state"
+import { useSearchParams } from "next/navigation"
+import { client } from "@/lib/client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card } from "@/components/ui/card"
+import { BarChart } from "lucide-react"
 
 interface CategoryPageContentProps {
   hasEvents: boolean
@@ -16,14 +21,87 @@ export const CategoryPageContent = ({
   hasEvents: initialHasEvents,
   category,
 }: CategoryPageContentProps) => {
+  const searchParams = useSearchParams()
+  const [activeTab, setActiveTab] = useState<"today" | "week" | "month">(
+    "today"
+  )
+  const page = parseInt(searchParams.get("page") || "1", 10) // use radix 10 to prase this into a number.
+  // http://localhost:3000/category/sale?page=5&limit=30 example
+  const limit = parseInt(searchParams.get("limit") || "30", 10)
+
+  const [pagination, setPagination] = useState({
+    pageIndex: page - 1,
+    pageSize: limit,
+  })
+
   const { data: pollingData } = useQuery({
     queryKey: ["category", category.name, "hasEvents"], // if the hash of the query key differs, the query is invalidated and refetched
     initialData: { hasEvents: initialHasEvents }, // we are polling the backend in an empty state
   }) // it doesn't not have queryFn because we are using this as a state
 
+  const { data, isFetching, isPending } = useQuery({
+    queryKey: [
+      "events",
+      category.name,
+      pagination.pageIndex,
+      pagination.pageSize,
+      activeTab,
+    ],
+    queryFn: async () => {
+      const res = await client.category.getEventsByCategoryName.$get({
+        name: category.name,
+        page: pagination.pageIndex + 1,
+        limit: pagination.pageSize,
+        timeRange: activeTab,
+      })
+
+      return await res.json()
+    },
+    refetchOnWindowFocus: false,
+    enabled: pollingData.hasEvents,
+  })
+
   if (!pollingData.hasEvents) {
     return <EmptyCategoryState categoryName={category.name} />
   }
 
-  return <div>CategoryPageContent</div>
+  return (
+    <div className="space-y-6">
+      <Tabs
+        value={activeTab}
+        onValueChange={(value) => {
+          setActiveTab(value as "today" | "week" | "month")
+        }}
+      >
+        <TabsList className="mb-2">
+          <TabsTrigger value="today">Today</TabsTrigger>
+          <TabsTrigger value="week">This Week</TabsTrigger>
+          <TabsTrigger value="month">This Month</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value={activeTab}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
+            <Card className="border-2 border-brand-700">
+              <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <p className="text-sm/6 font-medium">Total Events</p>
+                <BarChart className="size-4 text-muted-foreground" />
+              </div>
+
+              <div>
+                <p className="text-2xl font-bold">{data?.eventsCount || 0}</p>
+                <p className="text-xs/5 text-muted-foreground">
+                  Events{" "}
+                  {activeTab === "today"
+                    ? "today"
+                    : activeTab === "week"
+                      ? "this week"
+                      : "this month"}
+                </p>
+              </div>
+            </Card>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 }
